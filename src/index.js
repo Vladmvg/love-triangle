65b2f1a109adf3f4b75e3f9d471a1a7e6ff77e05
Vladmvg <vladmvg24@gmail.com>
2019-03-24 23:19:49 +0300

    Add triangles

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+	var sum = 0;  
+  for (var i = 0; i < preferences.length; i++) { 
+      var fir = preferences[i] - 1;
+      if (fir == i) continue;
+      var sec = preferences[fir] - 1;
+      if (fir == sec) continue;
+      var th = preferences[sec] - 1;
+      if (th == sec) continue;
+      if (th == i) {
+          sum++;
+      }
+   }
+   return sum / 3;
 };
